55a02b76e48d5b2765789468ff63e80e66323349
Jesper Bang <s144211@student.dtu.dk>
2021-12-20 09:19:55 +0100

    progress

--- exam dec 2019/ex02-library.cpp
+++ exam dec 2019/ex02-library.cpp
@@ -3,15 +3,22 @@
 
 //Exercise 2 (a) Implement this function
 string reverse(string s, int n){
-	//put your code here
+    if(s.size()-n != 0)
+    {
+        swap(s[s.size()-n],s[n]);
+		reverse(s,n-1);
+    } 
+	return s;
 }
 
 //Exercise 2 (b) Implement this function
 bool isPalindrome(string s, int n1, int n2){
+	return true;
 	//put your code here
 }
 
 //Exercise 2 (c) Implement this function
 int distancePalindrome(string s, int n1, int n2){
 	//put your code here
+	return 0;
 }
--- exam dec 2019/ex02-main.cpp
+++ exam dec 2019/ex02-main.cpp
@@ -8,17 +8,17 @@ void testReverse(string s){
 	cout << s << " | " << rev << endl;
 }
 
-void testPalindrome(string s){
-	bool pal = isPalindrome(s,0,s.length()-1);
-	cout << s << ((pal)?" is":" is not");
-	cout <<" palindrome" << endl;
-}
+// void testPalindrome(string s){
+// 	bool pal = isPalindrome(s,0,s.length()-1);
+// 	cout << s << ((pal)?" is":" is not");
+// 	cout <<" palindrome" << endl;
+// }
 
-void testDistancePalindrome(string s){
-	int k = distancePalindrome(s,0,s.length()-1);
-	cout << s << " requires " << k;
-	cout << " changes to be palindrome"<< endl;
-}
+// void testDistancePalindrome(string s){
+// 	int k = distancePalindrome(s,0,s.length()-1);
+// 	cout << s << " requires " << k;
+// 	cout << " changes to be palindrome"<< endl;
+// }
 
 int main() {
 	string words[] = {"","a","aa","ab","aba","abc",
@@ -30,15 +30,15 @@ int main() {
 		testReverse(words[i]);
 	}
 
-	cout << endl << "PALINDROME" << endl;
-	for(int i=0;i<length;i++){
-		testPalindrome(words[i]);
-	}
+	// cout << endl << "PALINDROME" << endl;
+	// for(int i=0;i<length;i++){
+	// 	testPalindrome(words[i]);
+	// }
 
-	cout << endl << "DISTANCE-PALINDROME" << endl;
-	for(int i=0;i<length;i++){
-		testDistancePalindrome(words[i]);
-	}
+	// cout << endl << "DISTANCE-PALINDROME" << endl;
+	// for(int i=0;i<length;i++){
+	// 	testDistancePalindrome(words[i]);
+	// }
 
 	return 0;
 }
